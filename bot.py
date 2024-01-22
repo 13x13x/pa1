4aecf290e175dbbfc6b4e89512cda60560d9e91f
GH <130484247+vvzsf@users.noreply.github.com>
2024-01-22 09:29:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -231,14 +231,6 @@ async def dbtool(_, m : Message):
 👥 Groups : `{x}`
 🚧 Total users & groups : `{tot}` """)
 
-@app.on_message(filters.chat(-1001947425388))
-async def forward_channel_post(_, message):
-    # Get the chat ID of the bot
-    bot_chat = await app.get_chat(chat_id=message.chat.id)
-    
-    # Forward the message from the channel to the bot
-    await app.copy_message(chat_id=bot_chat.id, from_chat_id=message.chat.id, message_id=message.message_id.message_id)
-
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Broadcast ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 @app.on_message(filters.command("bcast") & filters.user(cfg.SUDO))
