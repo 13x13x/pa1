479cc13828e4dbf1cfba0ee780eaf06d47be0b5c
1212x <153285185+1212x@users.noreply.github.com>
2024-06-03 20:00:43 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "24025974"))
     API_HASH = getenv("API_HASH", "2abf0406f41a57b540bdefe8b12d114f")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6615313068:AAGcPqcI4BxCw2ksGUGA1qkibkQJ_OMUN-w")
     FSUB = getenv("FSUB", "PanindiaFilmZ")
     CHID = int(getenv("CHID", "-1001930424346"))
     SUDO = list(map(int, getenv("SUDO").split()))
