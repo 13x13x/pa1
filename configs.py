e0e59ef85061a5aa9638647d09a80a9230c5bcf9
1212x <153285185+1212x@users.noreply.github.com>
2024-06-25 11:33:15 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-from os import path, getenv
+from os import getenv
 
 class Config:
     API_ID = int(getenv("API_ID", "24025974"))
@@ -6,7 +6,6 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "6615313068:AAGcPqcI4BxCw2ksGUGA1qkibkQJ_OMUN-w")
     FSUB = getenv("FSUB", "PanindiaFilmZ")
     CHID = int(getenv("CHID", "-1001930424346"))
-    SUDO = list(map(int, getenv("SUDO").split()))
+    SUDO = list(map(int, getenv("SUDO", "5549620776").split())) if getenv("SUDO") else []
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://vasanthtg8:vasanthtg8@cluster0.441zwwt.mongodb.net/?retryWrites=true&w=majority")
-    
 cfg = Config()
