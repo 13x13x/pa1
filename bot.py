051e26c3874cb17d3b3588f8b82faf0a1fc09fc2
13x13x <153285185+13x13x@users.noreply.github.com>
2024-07-07 08:30:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ async def op(_, m :Message):
                 ]
             )
             add_user(m.from_user.id)
-            await m.reply_photo("https://graph.org/file/18091323ff1c954bb5c97.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\nPowerd By : @PanindiaFilmZ**".format(m.from_user.mention, "https://t.me/PIFAdminBot"), reply_markup=keyboard)
+            await m.reply_photo("https://telegra.ph/file/19cd8a38aa67805bd5805.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\nPowerd By : @PanindiaFilmZ**".format(m.from_user.mention, "https://t.me/PIFAdminBot"), reply_markup=keyboard)
     
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
