8d2fc3beda117766b373b7fa3d84d1465b8108e4
13x13x <153285185+13x13x@users.noreply.github.com>
2024-11-12 21:05:36 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import getenv
 class Config:
     API_ID = int(getenv("API_ID", "24025974"))
     API_HASH = getenv("API_HASH", "2abf0406f41a57b540bdefe8b12d114f")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6615313068:AAHcvRg0j0jd_U0hJapBa3SNSKpeCRvYuj4")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6615313068:AAEWmvEX_142l0aLY3MbzvmKnuys6c0v0Ec")
     FSUB = getenv("FSUB", "PanindiaFilmZ")
     CHID = int(getenv("CHID", "-1001930424346"))
     SUDO = list(map(int, getenv("SUDO", "5549620776").split()))
